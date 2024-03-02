3944600bc6c7c27cf63011924fd35ef8d8ed2204
Rezaul <rkarim923@gmail.com>
2024-03-02 19:05:47 +0000

    update

--- config/index.js
+++ config/index.js
@@ -1,5 +1,5 @@
 const MONGODB_URL =
-  "mongodb+srv://imrezaulkrm:abc123456@nodejs.3f7msqz.mongodb.net/?retryWrites=true&w=majority&appName=nodejs";
+  "mongodb+srv://username-password@nodejs.3f7msqz.mongodb.net/?retryWrites=true&w=majority&appName=nodejs";
 const PORT = 5000;
 
 module.exports = { MONGODB_URL, PORT };
